b54174c76a657a2e67e0437c2a094d201c5b43f3
safron4ik-code <60510094+safron4ik-code@users.noreply.github.com>
2020-01-31 19:53:04 +0500

    Update xd_connection.js

--- xd_connection.js
+++ xd_connection.js
@@ -1,1 +1,10 @@
 <script src="https://vk.com/js/api/xd_connection.js?2"  type="text/javascript"></script>
+<script type="text/javascript">
+  VK.init(function() {
+     // API initialization succeeded
+     // Your code here
+  }, function() {
+     // API initialization failed
+     // Can reload page here
+}, '5.103');
+</script>
